0f2a3c0a60c88f82d5e722b982f58a6651e8ebcd
Felipe300 <caracoloro@gmail.com>
2023-06-05 09:15:48 -0400

    feat: add basic server

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+const PORT = 3000
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(PORT, () => console.log(`Server running on port => ${PORT} 🔥🔥🔥`))
